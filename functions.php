e5200d4b74fd462ece5a60c30432d1edcfb65971
Thomas Gollenia <thomas@gollenia.at>
2023-01-21 09:10:36 +0100

    stuff

--- functions.php
+++ functions.php
@@ -3,8 +3,9 @@
 $script = require_once(__DIR__ . "/version.php");
 add_action( 'wp_enqueue_scripts', function() use($script) {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' , [],
-	$script['version']
-);
+		$script['version']
+	);
+	wp_enqueue_style( 'material-icons', 'https://fonts.googleapis.com/icon?family=Material+Icons' 	);
 } );
 
 add_action( 'admin_enqueue_scripts', function() use($script) {
